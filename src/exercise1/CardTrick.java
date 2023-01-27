fe0341ccf1b5b2811c6a4811ad2d6cce9e517260
Kirujan Jegatheeswaran <jegathki@sheridancollege.ca>
2023-01-27 13:51:31 -0500

    New Branch for my work

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,5 @@
 package exercise1;
-
+import java.Util.Random;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -12,16 +12,16 @@ public class CardTrick {
     
     public static void main(String[] args) {
         
-        Card[] hand = new Card[7];
+        Card[] hand = new Card[7]; // an array is created with value of 7 for its index
 
-        for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
-            //card.setValue(insert call to random number generator here)
+        for (int i = 0; i < hand.length; i++) { // for loop
+            Card card = new Card(); // instantiate the new card object 
+           // card.setValue(make random generator);
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
-        }
+        } // end of for loop
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
