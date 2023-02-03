e98c81eb71850aa400265301406cf4705e22278d
Kirujan Jegatheeswaran <jegathki@sheridancollege.ca>
2023-02-03 13:58:14 -0500

    Updated with "Im done!"

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -61,6 +61,7 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
+        // Im done!
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
